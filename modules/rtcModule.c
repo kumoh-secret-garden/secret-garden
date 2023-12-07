520648d2a0090be66d47c5c2dd0ce17e3afdc017
JYeonJun <jyj3898@naver.com>
2023-12-07 11:49:02 +0900

    Refactor: rtcModule.c 코드 리팩토링

--- modules/rtcModule.c
+++ modules/rtcModule.c
@@ -4,111 +4,121 @@
 static const char *I2C_DEV = "/dev/i2c-1"; // I2C 연결을 위한 장치 파일
 
 // BCD를 10진수로 변환하는 함수
-int bcdToDec(int val);
+static int bcdToDec(int val);
 
-/*
-현재 시간을 받아오는 함수
-- 모니터링 기능에서 사용됨
-*/
-void *syncCurrentTime(void *arg)
+// I2C 설정 함수
+static int setupI2C();
+
+// 시간 갱신 함수
+static void updateTime(int i2c_fd);
+
+// 스케줄링 함수
+static void scheduleTask(time_t *lastTime, int interval, void *(*task)(void *), pthread_t *thread);
+
+// 현재 시간을 받아오는 함수
+void *syncCurrentTime(void *arg);
+
+// 특정 시간마다 다른 센서들을 실행시키는 스케줄러 함수
+void *scheduler(void *arg);
+
+// BCD를 10진수로 변환하는 함수
+static int bcdToDec(int val)
 {
-    int i2c_fd;
+    return ((val / 16 * 10) + (val % 16));
+}
 
+// I2C 설정 함수
+static int setupI2C()
+{
     if (wiringPiSetup() < 0)
     {
-        printf("wiringPiSetup() is failed");
+        printf("wiringPiSetup() is failed\n");
         return -1;
     }
 
-    i2c_fd = wiringPiI2CSetupInterface(I2C_DEV, SLAVE_ADDR_01);
+    int i2c_fd = wiringPiI2CSetupInterface(I2C_DEV, SLAVE_ADDR_01);
 
     if (i2c_fd == -1)
     {
-        printf("i2c_fd device setup failed");
+        printf("i2c_fd device setup failed\n");
         return -1;
     }
 
+    return i2c_fd;
+}
+
+// 시간 갱신 함수
+static void updateTime(int i2c_fd)
+{
+    struct tm timeinfo = {0};
+
+    // BCD 형태로 저장된 시간 정보를 10진수 형태로 변환
+    timeinfo.tm_year = bcdToDec(wiringPiI2CReadReg8(i2c_fd, YEAR_REG)) + 100; // tm_year는 1900년부터의 연도를 나타냅니다.
+    timeinfo.tm_mon = bcdToDec(wiringPiI2CReadReg8(i2c_fd, MONTH_REG)) - 1;   // tm_mon는 0부터 시작합니다.
+    timeinfo.tm_mday = bcdToDec(wiringPiI2CReadReg8(i2c_fd, DATE_REG));
+    timeinfo.tm_hour = bcdToDec(wiringPiI2CReadReg8(i2c_fd, HOUR_REG) & ~(1 << 6));
+    timeinfo.tm_min = bcdToDec(wiringPiI2CReadReg8(i2c_fd, MIN_REG));
+    timeinfo.tm_sec = bcdToDec(wiringPiI2CReadReg8(i2c_fd, SEC_REG));
+
+    // 뮤텍스 잠금
+    pthread_mutex_lock(&mtx_current_time);
+
+    // current_time 갱신
+    current_time = mktime(&timeinfo); // struct tm을 time_t로 변환
+
+    // 뮤텍스 해제
+    pthread_mutex_unlock(&mtx_current_time);
+}
+
+// 스케줄링 함수
+static void scheduleTask(time_t *lastTime, int interval, void *(*task)(void *), pthread_t *thread)
+{
+    // 뮤텍스 잠금
+    pthread_mutex_lock(&mtx_current_time);
+
+    // 안전하게 current_time 값 가져오기
+    time_t currentTime = current_time;
+
+    // 뮤텍스 해제
+    pthread_mutex_unlock(&mtx_current_time);
+
+    // 특정 기능은 interval 초마다 실행
+    if (difftime(currentTime, *lastTime) >= interval)
+    {
+        pthread_create(thread, NULL, task, NULL);
+        pthread_detach(*thread);
+        *lastTime = currentTime;
+    }
+}
+
+void *syncCurrentTime(void *arg)
+{
+    int i2c_fd = setupI2C();
+    if (i2c_fd < 0)
+        return NULL;
+
     while (1)
     {
-        struct tm timeinfo = {0};
-
-        // BCD 형태로 저장된 시간 정보를 10진수 형태로 변환
-        timeinfo.tm_year = bcdToDec(wiringPiI2CReadReg8(i2c_fd, YEAR_REG)) + 100; // tm_year는 1900년부터의 연도를 나타냅니다.
-        timeinfo.tm_mon = bcdToDec(wiringPiI2CReadReg8(i2c_fd, MONTH_REG)) - 1;   // tm_mon는 0부터 시작합니다.
-        timeinfo.tm_mday = bcdToDec(wiringPiI2CReadReg8(i2c_fd, DATE_REG));
-        timeinfo.tm_hour = bcdToDec(wiringPiI2CReadReg8(i2c_fd, HOUR_REG) & ~(1 << 6));
-        timeinfo.tm_min = bcdToDec(wiringPiI2CReadReg8(i2c_fd, MIN_REG));
-        timeinfo.tm_sec = bcdToDec(wiringPiI2CReadReg8(i2c_fd, SEC_REG));
-
-        // 뮤텍스 잠금
-        pthread_mutex_lock(&mtx_current_time);
-
-        // current_time 갱신
-        current_time = mktime(&timeinfo); // struct tm을 time_t로 변환
-
-        // 뮤텍스 해제
-        pthread_mutex_unlock(&mtx_current_time);
-
-        // 1초 대기
-        sleep(1);
+        updateTime(i2c_fd);
+        sleep(1); // 1초 대기
     }
 
     return NULL;
 }
 
-/*
-특정 시간마다 다른 센서들을 실행시키는 스케줄러 함수
-*/
 void *scheduler(void *arg)
 {
-    time_t lastMonitorTime = 0;
-    time_t lastRotateTime = 0;
-    time_t lastMusicTime = 0;
+    time_t lastMonitorTime = 0, lastRotateTime = 0, lastMusicTime = 0;
     pthread_t monitorThread, rotateThread, musicThread;
 
     while (1)
     {
+        scheduleTask(&lastMonitorTime, 30, monitor_plant, &monitorThread);
+        scheduleTask(&lastRotateTime, 60, rotate_pot, &rotateThread);
+        scheduleTask(&lastMusicTime, 180, play_music, &musicThread);
+
         sleep(1); // 1초마다 스케줄러 작동
-
-        // 뮤텍스 잠금
-        pthread_mutex_lock(&mtx_current_time);
-
-        // 안전하게 current_time 값 가져오기
-        time_t currentTime = current_time;
-
-        // 뮤텍스 해제
-        pthread_mutex_unlock(&mtx_current_time);
-
-        // 모니터링 기능은 30초마다 실행
-        if (difftime(currentTime, lastMonitorTime) >= 30)
-        {
-            pthread_create(&monitorThread, NULL, monitor_plant, NULL);
-            pthread_detach(monitorThread);
-            lastMonitorTime = currentTime;
-        }
-
-        // 화분 회전 기능은 1분마다 실행
-        if (difftime(currentTime, lastRotateTime) >= 60)
-        {
-            pthread_create(&rotateThread, NULL, rotate_pot, NULL);
-            pthread_detach(rotateThread);
-            lastRotateTime = currentTime;
-        }
-
-        // 음악재생 기능은 3분마다 실행
-        if (difftime(currentTime, lastMusicTime) >= 180)
-        {
-            pthread_create(&musicThread, NULL, play_music, NULL);
-            pthread_detach(musicThread);
-            lastMusicTime = currentTime;
-        }
     }
 
     return NULL;
-}
-
-// BCD를 10진수로 변환하는 함수
-int bcdToDec(int val)
-{
-    return ((val / 16 * 10) + (val % 16));
 }
