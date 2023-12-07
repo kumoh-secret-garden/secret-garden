abbe3e13a1d20b4db798bd69e75697564233f87c
JYeonJun <jyj3898@naver.com>
2023-12-07 21:10:20 +0900

    Feat: 물주기 기능 구현 완료

--- modules/waterPlant.c
+++ modules/waterPlant.c
@@ -1,8 +1,96 @@
 #include "waterPlant.h"
+#include <wiringPiSPI.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <time.h>
+
+#define SLAVE_ADDR 0x48
+#define I2C_DEV "/dev/i2c-1"
+#define ADC_CHANNEL 2
+
+int init_I2C()
+{
+    if (wiringPiSetupGpio() < 0)
+    {
+        printf("Failed to set up wiringPi");
+        return -1;
+    }
+
+    int i2c_fd = wiringPiI2CSetupInterface(I2C_DEV, SLAVE_ADDR);
+    if (i2c_fd < 0)
+    {
+        printf("Failed to set up I2C");
+    }
+
+    return i2c_fd;
+}
+
+void setup_water_pump()
+{
+    pinMode(WATER_PUMP_PIN, OUTPUT); // 워터 펌프 핀을 출력 모드로 설정
+}
+
+void control_water_pump(float percent)
+{
+    if (percent < 50)
+    {
+        turnOnWaterPump();
+    }
+    else
+    {
+        turnOffWaterPump();
+    }
+}
+
+void turnOnWaterPump()
+{
+    digitalWrite(WATER_PUMP_PIN, HIGH); // 워터 펌프 켜기
+    printf("Water pump is turned on.");
+}
+
+void turnOffWaterPump()
+{
+    digitalWrite(WATER_PUMP_PIN, LOW); // 워터 펌프 끄기
+    printf("Water pump is turned off.");
+}
 
 /*
 물주기 기능(토양의 수분을 측정한 물주기 기능)
+by 정연준
 */
-void* water_plant(void* arg){
-    
+void *water_plant(void *arg)
+{
+    int i2c_fd = init_I2C();
+    if (i2c_fd >= 0)
+    {
+        printf("물주기 기능 started....");
+        setup_water_pump();
+        int cnt = 0;
+        while (1)
+        {
+            wiringPiI2CWrite(i2c_fd, 0x40 | ADC_CHANNEL);
+            int preVal = wiringPiI2CRead(i2c_fd);
+            int curVal = wiringPiI2CRead(i2c_fd);
+            float percent = 100 - ((float)curVal / 255) * 100;
+            printf("[%d] Previous value = %d", cnt, preVal);
+            printf("Current value = %d", curVal);
+            printf("Soil Moisture Percent: %.2f%\n", percent);
+
+            // 뮤텍스 잠금
+            pthread_mutex_lock(&mtx_soil_moisture);
+
+            // soil_moisture 갱신
+            soil_moisture = percent;
+
+            // 뮤텍스 해제
+            pthread_mutex_unlock(&mtx_soil_moisture);
+
+            // 워터 펌프 제어
+            control_water_pump(percent);
+
+            sleep(60);
+            cnt++;
+        }
+    }
+    return NULL;
 }
