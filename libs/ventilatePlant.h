6c16aefc97655ea0b340021b789c225cfc4c20fc
NoMaL21 <dntjd8509@naver.com>
2023-12-08 13:26:45 +0900

    Feat: 온습도 센서 데이터를 리턴하는 tempHumidSensor() 구현

--- libs/ventilatePlant.h
+++ libs/ventilatePlant.h
@@ -2,14 +2,15 @@
 #define VENTILATE_PLANT_H
 
 #include "common.h"
-
+ 
 #define MOTOR_PWM_PIN1 18 // 실제로 DC 모터의 PWM 핀이 연결된 GPIO 핀 번호
 #define MOTOR_PWM_PIN2 19 // 실제로 DC 모터의 PWM 핀이 연결된 GPIO 핀 번호
 
 /*
 환풍 기능(증산 작용 활성화를 위한 환풍 기능)
 by 조우성
+-파라미터의 값만큼의 속도로 dc모터가 회전함(20~30추천)
 */
-void *ventilate_plant(void *arg);
+void *ventilate_plant(int arg);
 
 #endif
