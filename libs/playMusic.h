36fb0f66c0162a626f8efa38f5ac71cb6f3dd92b
Journeybongbong <wlgus4110@gmail.com>
2023-12-08 14:25:38 +0900

    Feat: add playMusic

--- libs/playMusic.h
+++ libs/playMusic.h
@@ -2,10 +2,24 @@
 #define PLAY_MUSIC_H
 
 #include "common.h"
+#include <softTone.h>
+#include <time.h>
 
 #define BUZZER_GPIO 26
-#define RANGE 100
-#define DUTY 50
+#define TOTAL_NOTES 12
+#define SONG_COUNT 3
+
+// 계이름 표 구조
+typedef struct {
+    char* note_name;
+    float frequency;
+} Note;
+
+// 노래 목록 구조
+typedef struct{
+    int *notes;
+    int length;
+} Song;
 
 /*
 음악 재생 기능(특정 시간마다 음악을 재생하는 기능)
