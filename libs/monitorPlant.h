2a45f5069e22577bf1518fc5034c59c610953298
JYeonJun <jyj3898@naver.com>
2023-12-11 15:38:15 +0900

    코드 통합

--- libs/monitorPlant.h
+++ libs/monitorPlant.h
@@ -2,10 +2,17 @@
 #define MONITOR_PLANT_H
 
 #include "common.h"
+#include <stdio.h>
+#include <unistd.h>
+#include <wiringPi.h>
+#include <wiringSerial.h>
+#include <time.h>
 
 #define BAUD_RATE 115200 // 블루투스의 보율이 바뀔 경우 이 값을 변경해야 한다.
 #define UART_TX_GPIO 0   // 전송(TX)을 위한 핀
 #define UART_RX_GPIO 1   // 수신(RX)을 위한 핀
+
+#define UART_DEV = "/dev/ttyAMA1";
 
 /*
 식물 상태 모니터링 기능(온도, 습도, 토양 수분, 시간 정보 제공 기능)
--- modules/rotatePot.c
+++ modules/rotatePot.c
@@ -1,68 +1,70 @@
 #include "rotatePot.h"
 
-
-#define CW 0
-#define CCW 1
-
 int steps = (4096 * 10) / 360;
 
+int one_two_phase[8][4] = {{1, 0, 0, 0}, {1, 1, 0, 0}, {0, 1, 0, 0}, {0, 1, 1, 0}, {0, 0, 1, 0}, {0, 0, 1, 1}, {0, 0, 0, 1}, {1, 0, 0, 1}};
 
-int one_two_phase[8][4] 
-= {{1, 0, 0, 0}, {1, 1, 0, 0},
-{0, 1, 0, 0}, {0, 1, 1, 0},
-{0, 0, 1, 0}, {0, 0, 1, 1},
-{0, 0, 0, 1}, {1, 0, 0, 1}};
-
-
-void init_step() {
-    for(int i = 0; i< 4; i++){
+void init_step()
+{
+    for (int i = 0; i < 4; i++)
+    {
         pinMode(GPIO_ARR[i], OUTPUT);
     }
 }
 
-void rotate_cw() {
-    for(int i = 0; i < steps; i++) {
-        digitalWrite(GPIO_ARR[3], one_two_phase[i%8][0]);
-        digitalWrite(GPIO_ARR[2], one_two_phase[i%8][1]);
-        digitalWrite(GPIO_ARR[1], one_two_phase[i%8][2]);
-        digitalWrite(GPIO_ARR[0], one_two_phase[i%8][3]);
+void rotate_cw()
+{
+    for (int i = 0; i < steps; i++)
+    {
+        digitalWrite(GPIO_ARR[3], one_two_phase[i % 8][0]);
+        digitalWrite(GPIO_ARR[2], one_two_phase[i % 8][1]);
+        digitalWrite(GPIO_ARR[1], one_two_phase[i % 8][2]);
+        digitalWrite(GPIO_ARR[0], one_two_phase[i % 8][3]);
         delay(2);
     }
 }
 
-void rotate_ccw() {
-    for(int i = 0; i < steps; i++) {
-        digitalWrite(GPIO_ARR[0], one_two_phase[i%8][0]);
-        digitalWrite(GPIO_ARR[1], one_two_phase[i%8][1]);
-        digitalWrite(GPIO_ARR[2], one_two_phase[i%8][2]);
-        digitalWrite(GPIO_ARR[3], one_two_phase[i%8][3]);
+void rotate_ccw()
+{
+    for (int i = 0; i < steps; i++)
+    {
+        digitalWrite(GPIO_ARR[0], one_two_phase[i % 8][0]);
+        digitalWrite(GPIO_ARR[1], one_two_phase[i % 8][1]);
+        digitalWrite(GPIO_ARR[2], one_two_phase[i % 8][2]);
+        digitalWrite(GPIO_ARR[3], one_two_phase[i % 8][3]);
         delay(2);
     }
 }
 
-void one_two_phase_rotate_angle() {
+void one_two_phase_rotate_angle()
+{
     printf("angle = %d\n", angle);
 
-    
-   
-    if(direction == 0) { // CW
+    if (direction == CW)
+    { // CW
 
-        if(angle < 180) {
+        if (angle < 180)
+        {
             rotate_cw();
             angle += 10;
         }
-        else {
+        else
+        {
             direction = CCW;
             rotate_ccw();
             angle -= 10;
         }
-    } else if(direction == 1) { // CCW
+    }
+    else if (direction == CCW)
+    { // CCW
 
-        if(angle > -180) {
+        if (angle > -180)
+        {
             rotate_ccw();
             angle -= 10;
         }
-        else {
+        else
+        {
             direction = CW;
             rotate_cw();
             angle += 10;
@@ -73,10 +75,11 @@ void one_two_phase_rotate_angle() {
 /*
 화분 회전 기능(빛이 골고루 닿을 수 있도록 화분을 회전하는 기능)
 */
-void* rotate_pot(void* arg){
+void *rotate_pot(void *arg)
+{
     // RTC 모듈에 의해 호출됨
-    wiringPiSetupGpio();
+    // wiringPiSetupGpio();
     init_step();
-    
+
     one_two_phase_rotate_angle();
 }
